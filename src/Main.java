22cd61d68a781b324e12961a1e13603e2c67364b
Aizada30 <131680193+Aizada30@users.noreply.github.com>
2023-05-02 12:36:45 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,31 +2,26 @@ public class Main {
     public static void main(String[] args){
 
 
-//       Triangle triangle = new Triangle();//Main
-//       triangle.a=5;
-//       triangle.b=3;
-//       triangle.c=4;
-//
-//       triangle.area();
-       /** MyClass деген класс тузунуз
+      Triangle triangle = new Triangle();//Main
+      triangle.a=5;
+      triangle.b=3;
+      triangle.c=4;
 
-        Ал класста озунуз жонундо маалымат сактаган полелер болсун(атыныз, фамилияныз, жашыныз, Peaksoft то катышкан сабактарыныз(массив), жакшы коргон тамагыныз)
+      triangle.area();
+//        * MyClass деген класс тузунуз
 
-        Параметри эки башка болгон эки конструктор тузунуз
+//         Ал класста озунуз жонундо маалымат сактаган полелер болсун(атыныз, фамилияныз, жашыныз, Peaksoft то катышкан сабактарыныз(массив), жакшы коргон тамагыныз)
 
-        MyClassтын 2 объектисин тузуп, конструктор аркылуу маани бериниз
+//         Параметри эки башка болгон эки конструктор тузунуз
 
-        Эки объектке эки башка конструктор иштесин.
+//         MyClassтын 2 объектисин тузуп, конструктор аркылуу маани бериниз
 
-        Объекттердин маанилерин консольго чыгарыныз.*/
+//         Эки объектке эки башка конструктор иштесин.
+
+//         Объекттердин маанилерин консольго чыгарыныз.
 
 
-        Myclass myclass = new Myclass();
-        myclass.nameofPerson="Aizada";
-        myclass.lastname="Abdyrazakova";
-        myclass.age=20;
-        myclass.tableofPeaksoft  =new String[]{"prak","softskills","english"} ;
-        myclass.nameofCar="Bugaty";
+      
     }
 
 
